81fe629688bfce644ad4b928676df250460d64dd
PL3B3 <kevinqiesq@gmail.com>
2020-07-16 02:38:46 -0500

    Added phantom die and respawn functions handling client sync

--- ChubbyServer0/character/base_character/ChubbyPhantom.gd
+++ ChubbyServer0/character/base_character/ChubbyPhantom.gd
@@ -25,13 +25,17 @@ func _ready():
 # UNRELIABLE protocol
 # helper function for updating a node attribute (on clients) based on new server info
 func send_updated_attribute(node_name: String, attribute_name: String, new_value) -> void:
-	server.send_server_rpc_to_all_players_unreliable("update_node_attribute", [node_name, attribute_name, new_value])
+	server.send_server_rpc_to_all_players_unreliable("update_node_attribute", [node_name, attribute_name, new_value]
 
+# call a method on this phantom's client instance
+func replicate_on_client(method_name: String, args) -> void:
+	server.send_server_rpc_to_all_players("call_player_method", [player_id, method_name, args])       
 
 # calls a method on this phantom and all its client instances
 func call_and_sync(method_name: String, args) -> void: 
 	callv(method_name, args)
-	server.send_server_rpc_to_all_players("call_player_method", [player_id, method_name, args])
+	replicate_on_client(method_name, args)
+
 
 # 1. call the ability with arguments passed in, tba at time of button press
 # 2. activate cooldown timer
@@ -61,3 +65,17 @@ func send_updates():
 	send_updated_attribute(str(player_id), "velocity", velocity)
 	send_updated_attribute(str(player_id), "position", position)
 	send_updated_attribute(str(player_id), "rot_angle", rot_angle)
+
+func die():
+       # performs death functionality
+       .die()
+       # tells client to die
+       replicate_on_client("die", [])
+       # starts respawn timer
+       add_and_return_timed_effect_exit("call_and_sync", ["respawn", []], 10)
+
+func respawn():
+       # performs respawn functionality
+       .respawn()
+       # tell client to respawn
+       replicate_on_client("respawn", [])
